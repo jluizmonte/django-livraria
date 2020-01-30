96cb99a2c53d15a66ee9f1a15c1daca2d73131ce
Felipe <felipe.alves2014@gmail.com>
2020-01-29 21:49:33 -0300

    adição de teste para app shoping-cart

--- apps/shopping_cart/tests.py
+++ apps/shopping_cart/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+from apps.book.models import Book
+from .models import ShoppingCart
 
-# Create your tests here.
+
+class UserTest(TestCase):
+
+    def test_add(self):
+        user = User.objects.create(username='joedoe', password='ArthurTuring', email='joedoe@admin.com')
+        self.client.force_login(user)
+        book = Book.objects.create(name='Jane Doe', description='teste', price=10)
+        response = self.client.get(reverse('shopping-cart-add', kwargs={'book':book.pk}))
+        ShoppingCart.objects.get(user=user)
+        self.assertEquals(response.status_code, 302)
+
+    def test_remove(self):
+        user = User.objects.create(username='joedoe', password='ArthurTuring', email='joedoe@admin.com')
+        self.client.force_login(user)
+        book = Book.objects.create(name='Jane Doe', description='teste', price=10)
+        shopping_cart = ShoppingCart.objects.create(user=user)
+        shopping_cart.book.add(book)
+        response = self.client.get(reverse('shopping-cart-remove', kwargs={'book': book.pk}))
+        ShoppingCart.objects.get(user=user)
+        self.assertEquals(response.status_code, 302)
